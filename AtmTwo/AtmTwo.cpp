e44f689e93ba8bb415cd8a824068939cdc20426d
Andrei Gridin <andreigridin918@gmail.com>
2023-10-14 21:01:32 +0500

    Добавил комментарии

--- AtmTwo/AtmTwo.cpp
+++ AtmTwo/AtmTwo.cpp
@@ -9,41 +9,41 @@ int main()
     system("chcp 1251 > nul");
 
     cout << endl;
-    int const banknote1 = 100;
+    int const banknote1 = 100;  // Объявляем и инициализируем константные пременные с наминалом купюр
     int const banknote2 = 200;
     int const banknote3 = 500;
     int const banknote4 = 1000;
     int const banknote5 = 2000;
     int const banknote6 = 5000;
-    int limit = 150000;
-    int summ = 0;
-    int numberOfBanknotes = 0;
+    int const limit = 150000;  // Объявляем и инициализируем константную пременную лимита выдачи суммы
+    int summ = 0;              // Объявляем и инициализируем пременную для записи суммы запрашиваеммой в банкомате
+    int numberOfBanknotes = 0; // Объявляем и инициализируем пременную для записи ко-ва купюр
 
-    cout << " Введите сумму к выдаче: ";
+    cout << " Введите сумму к выдаче: "; // Запрашиваем и записываем в переменную summ сумму для выдачи
     cin >> summ;
 
-    if (summ > limit) {
+    if (summ > limit) {                  // Проверяем лимит выдаваемый банкоматом
         cout << " Превышен лимит выдачи банкоматом!" << endl;
     }
     else {
-        if (summ <= 0) {
+        if (summ <= 0) {                 // Проверяем корректность введённой суммы
             cout << " Некорректно введена сумма!" << endl;
         }
         else {
-            if (summ % banknote1 != 0) {
-                cout << " Банкомат не может выдать " << summ << " рублей!" << endl;
+            if (summ % banknote1 != 0) { // Проверяем кратность запрашиваемой суммы 100 рублёвой банкноте
+                cout << " Банкомат не может выдать " << summ << " рублей!" << endl; // Если не кратна выводим сообщение о не возможности данной операции
                 cout << " Введите сумму кратную " << banknote1 << " рублям." << endl;
             }
-            else {
-                numberOfBanknotes = summ / banknote6;
-                if (numberOfBanknotes > 0) {
+            else {  // Иначе
+                numberOfBanknotes = summ / banknote6; // Делим сумму на банкноты в 5000 рублей и если кол-во банктнот больше нуля то выводим
+                if (numberOfBanknotes > 0) {          // сообщение о кол-ве банкнот с данным наминалом
                     cout << numberOfBanknotes << " банкнот достоинством " << banknote6 << " рублей." << endl;
                 }
-                numberOfBanknotes = summ % banknote6 / banknote5;
-                if (numberOfBanknotes > 0) {
-                    cout << numberOfBanknotes << " банкнот достоинством " << banknote5 << " рублей." << endl;
+                numberOfBanknotes = summ % banknote6 / banknote5; // Вычисляем остаток от суммы делённый на 5000 купюры, деление остатка на 2000 купюры
+                if (numberOfBanknotes > 0) {                      // вычисляем кол-во 2000 купюр и если их больше нуля то выводим сообщение
+                    cout << numberOfBanknotes << " банкнот достоинством " << banknote5 << " рублей." << endl;//  о кол-ве банкнот с данным наминалом
                 }
-                numberOfBanknotes = summ % banknote6 % banknote5 / banknote4;
+                numberOfBanknotes = summ % banknote6 % banknote5 / banknote4; // Вышеизложенное производим для остальных купюр
                 if (numberOfBanknotes > 0) {
                     cout << numberOfBanknotes << " банкнот достоинством " << banknote4 << " рублей." << endl;
                 }
